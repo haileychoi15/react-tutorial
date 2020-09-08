4b09a8a5f229bae5688196fa4fe23fb88242eedc
yuyeong choi <imdud0612@gmail.com>
2020-09-09 05:16:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          리액트 잘부탁해 나는 유영이얌 ㅎㅎ
         </p>
         <a
           className="App-link"
