8ce372dd8f35757a36863bccd110e92d4796edb6
yuyeong choi <imdud0612@gmail.com>
2020-09-13 02:44:21 +0900

    배열에 항목 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
-import React from 'react';
-/*import React, { Component } from 'react';*/
-import Hello from './hello';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  }
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  /* nextId 값을 useref 를 사용하여 관리합니다. 변수의 값이 바뀌어도 컴포넌트가 리랜더링 되지 않습니다. */
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id:nextId.current,
+      username,
+      email,
+    }
+
+    setUsers(users.concat(user)); /* [...users, user] */
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+
+    nextId.current += 1;
+  }
+
   return (
-    <UserList />
+      <>
+        <CreateUser
+            username={username}
+            email={email}
+            onCreate={onCreate}
+            onChange={onChange}>
+        </CreateUser>
+        <UserList users={users} />
+      </>
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 /* 배열 랜더링하기 */
 
-function User({ user }) {
+function User({ user }) { /* App.js 에서 props로 받아옵니다. */
     return (
         <div>
             <b>{user.username}</b> <span>({user.email})</span>
@@ -10,30 +10,13 @@ function User({ user }) {
     );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
-
+function UserList({ users }) {
     return (
         <div>
             {
                 users.map(
                     user => (<User user={user} key={user.id}></User>)
+                    /* 고유 값이 없고 데이터가 적을 때에는 key 값으로 map()의 index를 넣을 수 있습니다. */
                 )
             }
         </div>
