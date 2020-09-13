35bd28567ffc800492545f90e27bdb1696b760a5
yuyeong choi <imdud0612@gmail.com>
2020-09-14 04:40:51 +0900

    Context API

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Counter from "./Counter";
+import ContextSample from "./ContextSample";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ContextSample />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
