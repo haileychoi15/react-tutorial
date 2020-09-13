5bd6261ab3fa2b55c307aa4f92a770996359b253
yuyeong choi <imdud0612@gmail.com>
2020-09-13 22:42:54 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
+import React, { useRef, useState, useReducer, useMemo, useCallback } from 'react';
 import './App.css';
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
@@ -8,7 +8,125 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
+const initialState = {
+  inputs: {
+    username: '',
+    email: '',
+  },
+  users: [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users: state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        )
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      throw new Error('Unhandled action');
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const nextId = useRef(4);
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    });
+  }, []);
+
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
+    });
+    nextId.current += 1;
+  }, [username, email]);
+
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+    /*console.log('id : ',id);*/
+  }, []);
+
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+    /*console.log('id : ',id);*/
+  }, []);
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
+  return (
+      <>
+        <CreateUser
+            username={username}
+            email={email}
+            onChange={onChange}
+            onCreate={onCreate}
+        />
+        <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
+        <div>활성 사용자 수 : {count}</div>
+      </>
+  );
+}
+
+/*
   const [inputs, setInputs] = useState({
     username: '',
     email: '',
@@ -46,7 +164,7 @@ function App() {
   ]);
 
 
-  /* nextId 값을 useref 를 사용하여 관리합니다. 변수의 값이 바뀌어도 컴포넌트가 리랜더링 되지 않습니다. */
+  /!* nextId 값을 useref 를 사용하여 관리합니다. 변수의 값이 바뀌어도 컴포넌트가 리랜더링 되지 않습니다. *!/
   const nextId = useRef(4);
 
   const onCreate = useCallback(() => {
@@ -56,11 +174,11 @@ function App() {
       email,
     }
 
-    setUsers(users => users.concat(user)); /* [...users, user] 동일한 방법 */
+    setUsers(users => users.concat(user)); /!* [...users, user] 동일한 방법 *!/
 
-/*
+/!*
     여기서 함수형 업데이트를 하면 users가 콜백으로써 최신 users를 참조 하기 때문에 deps에 users를 안넣어줘도 된다.
-*/
+*!/
 
     setInputs({
       username: '',
@@ -69,9 +187,9 @@ function App() {
 
     nextId.current += 1;
   }, [username, email]);
-/*
+/!*
   여기서 users가 빠지면 onCreate함수는 username과 email이 바뀔 때에만 다시 만들어진다.
-*/
+*!/
 
   const onRemove = useCallback(id => {
       setUsers(users => users.filter(user => user.id !== id));
@@ -84,6 +202,7 @@ function App() {
   }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
+
 
   return (
       <>
@@ -98,5 +217,5 @@ function App() {
       </>
   );
 }
-
+*/
 export default App;
