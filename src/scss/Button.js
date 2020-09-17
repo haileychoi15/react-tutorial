cb23e2c10bce4ee992bc3262b5b9449972183cd8
yuyeong choi <imdud0612@gmail.com>
2020-09-18 03:08:49 +0900

    style buttons

--- src/scss/Button.js
+++ src/scss/Button.js
@@ -4,12 +4,19 @@ import classNames from 'classnames';
 import './Button.scss';
 
 // size: large, medium, small
-function Button({ children, size }) {
-  return <button className={classNames('Button', size)}>{children}</button>
+// color: blue, pink, gray
+function Button({ children, size, color, outline, fullWidth }) {
+  return <button className={classNames('Button', size, color, {
+    outline, fullWidth
+  })}>{children}</button>
 }
 
+// outline와 fullWidth은 boolean 타입이기때문에 파라미터로 안넘어오면 undefined이다.
+// 그러면 classNames에서 처리하지 않기 때문에 기본값 설정해 줄 필요없다.
+
 Button.defaultProps = {
-  size: 'medium'
+  size: 'medium',
+  color: 'blue'
 };
 
 export default Button;
