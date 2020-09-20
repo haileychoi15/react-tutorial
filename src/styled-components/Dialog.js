c03335c43e6c20fef515e7e0cf3995275aba2ab1
yuyeong choi <imdud0612@gmail.com>
2020-09-21 00:26:44 +0900

    modal transition

--- src/styled-components/Dialog.js
+++ src/styled-components/Dialog.js
@@ -1,7 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes, css } from 'styled-components';
 import Button from './Button';
 
+
+const fadeIn = keyframes`
+ from {
+   opacity: 0;
+ }
+ to {
+   opacity: 1;
+ }
+`;
+
+const fadeOut = keyframes`
+ from {
+   opacity: 1;
+ }
+ to {
+   opacity: 0;
+ }
+`;
+
+const slideUp = keyframes`
+ from {
+   transform: translateY(200px);
+ }
+ to {
+   transform: translateY(0);
+ }
+`;
+
+const slideDown = keyframes`
+ from {
+   transform: translateY(0);
+ }
+ to {
+   transform: translateY(200px);
+ }
+`;
 
 const DarkBackground = styled.div`
   position: fixed;
@@ -13,6 +49,17 @@ const DarkBackground = styled.div`
   justify-content: center;
   align-items: center;
   background: rgba(0, 0, 0, 0.8);
+  
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${fadeIn};
+  animation-fill-mode: forwards;
+  
+  ${props => 
+    props.disappear &&
+    css`
+      animation-name: ${fadeOut};
+    `}
 `;
 
 const DialogBlock = styled.div`
@@ -29,6 +76,18 @@ const DialogBlock = styled.div`
   p {
     font-size: 1.125rem;
   }
+  
+  animation-duration: 0.25s;
+  animation-timing-function: ease-out;
+  animation-name: ${slideUp};
+  animation-fill-mode: forwards;
+  
+  ${props => 
+    props.disappear && 
+    css`
+      animation-name: ${slideDown};
+    `}
+  
 `;
 
 const ButtonGroup = styled.div`
@@ -45,10 +104,25 @@ const ShortMarginButton = styled(Button)` // 버튼을 상속받아서 수정
 `;
 
 function Dialog({ title, children, confirmText, cancelText, visible, onConfirm, onCancel }) {
-    if(!visible) return null;
+
+    const [animate, setAnimate] = useState(false);
+    const [localVisible, setLocalVisible] = useState(visible);
+
+    useEffect(() => {
+        // visible true -> false 상태가 바뀌는 중
+        // 즉, 모달 창 끄기 버튼을 눌러서 visible은 false가 되었지만, localVisible은 값은 아직 설정안해서 true인 상태
+        if(localVisible && !visible){
+            setAnimate(true);
+            setTimeout(() => setAnimate(false), 250);
+        }
+        setLocalVisible(visible);
+    }, [localVisible, visible]);
+
+    if(!localVisible && !animate) return null;
+
     return (
-        <DarkBackground>
-            <DialogBlock>
+        <DarkBackground disappear={!visible}>
+            <DialogBlock disappear={!visible}>
                 <h3>{title}</h3>
                 <p>{children}</p>
                 <ButtonGroup>
